7f3103c9c10e318478da0ad3431fc24c666701e5
Bababooey - aka Douglas Gaddum <DG246670@falmouth.ac.uk>
2024-09-07 16:31:35 +0100

    it begins

--- Assets/Chlo/C++Scripts/C++Stuff/test.cc
+++ Assets/Chlo/C++Scripts/C++Stuff/test.cc
@@ -1,7 +1,66 @@
 #include "test.h"
+#include <iostream>
+using namespace std;
+using UnityEngine;
+using System;
+
+template <matrix T>
+
+class Edge {
+public:
+Node* returnAttatchedNodes(int index){
+		return children[index];
+}
+//Bad method? we will see \(o.o)/
+int returnIndexInMatrix(matrix currentMatrix){
+return currentMatrix[grabMatrixPosition(returnAttatchedNodes(0)];
+}
+
+private:
+Node* attachedNodes[2];
+friend matrix createTree(int); 
+}
+
+class Node {
+for(var i = 0; i < 4; i++){
+adjacentEdges[i] = nullptr;
+}
+public:
+Edge* returnAdjacentEdges(int index){
+return adjacentEdges[index];
+}
+
+
+
+private:
+Edge* adjacentEdges[4];
+
+
+
+
+}
+
+
+
+extern "C" {
+DLL_EXPORT matrix changeMatrixAndReturn(matrix input){
+
+
+
+
+
+} 
+
+}
+
 
 extern "C" {
 int DLL_EXPORT returnTenPlusWhatever(int a){
 return a + 10;
     }
+// Prototype shape within matrix creation
+
+
+
+
 }
