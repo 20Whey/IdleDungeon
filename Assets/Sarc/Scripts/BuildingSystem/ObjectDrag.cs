8ff647513ca0f32f6285297759771086266ee5f9
DrKend <michael.wood2002@Gmail.com>
2024-09-27 20:24:38 +0100

    fixed sprite offset

--- Assets/Sarc/Scripts/BuildingSystem/ObjectDrag.cs
+++ Assets/Sarc/Scripts/BuildingSystem/ObjectDrag.cs
@@ -14,10 +14,11 @@ public class ObjectDrag : MonoBehaviour
     private void Update()
     {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 pos = new(mousePos.x, mousePos.y);
-
+        Vector3 pos = new Vector3(mousePos.x, mousePos.y);
         Vector3Int cellPos = BuildingSystem.Instance.GridLayout.WorldToCell(pos);
-        transform.position = BuildingSystem.Instance.GridLayout.CellToLocalInterpolated(cellPos);
+        Vector3 spritePos = BuildingSystem.Instance.GridLayout.CellToLocalInterpolated(cellPos);
+        spritePos = new Vector3(spritePos.x + 0.5f, spritePos.y + 0.5f);
+        transform.position = spritePos;
     }
 
     private void LateUpdate()
