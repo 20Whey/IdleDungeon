ea2c0bb6cebd6a14af8b445a20cfbdafbb910e1e
DrKend <michael.wood2002@Gmail.com>
2024-09-28 17:34:23 +0100

    simpleGoldCount

---
+++ Assets/Mike/Scripts/TreasureHoard.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureHoard : MonoBehaviour
+{
+	int goldIncreaseRate = 10;
+
+	private void Start()
+	{
+		InvokeRepeating("GoldTick", 1f, 3f);
+	}
+
+	private void GoldTick()
+	{
+		GoldManager.instance.IncreaseGold(goldIncreaseRate);
+	}
+}
