d6bf2bec1b13bb7acdaed7d9f5248213ebf58431
sarconia <cool_spy2@hotmail.com>
2024-10-17 12:38:53 -0300

    Added Gamepad/Keyboard navigation to the Menu

--- Assets/Sarc/Scripts/BuildingSystem/UI/SelectionMenuUI.cs
+++ Assets/Sarc/Scripts/BuildingSystem/UI/SelectionMenuUI.cs
@@ -5,106 +5,121 @@ using UnityEngine.UI;
 
 public class SelectionMenuUI : MonoBehaviour
 {
+    [Header("First Selected Option")]
+    [SerializeField] private GameObject firstContainer;
 
-    [Header("Both Lists must be on the same order")]
+    [Header("Both Lists must be on the same order as in Hierarchy")]
     [SerializeField] private Transform tabContainer;
     [SerializeField] private Transform placeablesContainer;
-    [Space]
 
-    [SerializeField] private List<Button> tabs;
-    [SerializeField] private List<Transform> placeablesContainers;
-    [SerializeField] private List<Button> placeablesList;
+    [Header("Shown for Debug Purposes")]
+    [SerializeField] private List<Button> tabs = new List<Button>();
+    [SerializeField] private List<Transform> placeablesContainers = new List<Transform>();
+    [SerializeField] private List<Button> placeablesList = new List<Button>();
 
-    //TODO: Change magic Numbers for a check of stablished width on the scene
-    private float selectedWidth = 271f;
-    private float deSelectedWidth = 217f;
+    private const float SelectedWidth = 271f;
+    private const float DeSelectedWidth = 217f;
 
     private void Start()
     {
-        int tabIndex;
+        InitializeTabs();
+        InitializePlaceables();
 
+        #region Activate First Container
+        DeactivateAllContainers();
+        ActivateContainer(0);
+        EventSystem.current.SetSelectedGameObject(firstContainer);
+        #endregion
+    }
+    private void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject != null) {
+            Button selectedTab = EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
+            if (selectedTab != null) {
+                int tabIndex = tabs.IndexOf(selectedTab);
+                if (tabIndex >= 0) {
+                    HandleTabSelection(tabIndex);
+                }
+            }
+        }
+    }
+
+    private void InitializeTabs()
+    {
         tabs.AddRange(tabContainer.GetComponentsInChildren<Button>());
 
+        foreach (var tab in tabs) {
+            tab.onClick.AddListener(() => OnTabSelected(tab));
+        }
+    }
+
+    private void InitializePlaceables()
+    {
         placeablesList.AddRange(placeablesContainer.GetComponentsInChildren<Button>());
 
-        placeablesList.ForEach(button => {
-            PlaceableButton unitButton = button.GetComponent<PlaceableButton>();
-
-            if (unitButton != null) {
+        foreach (var button in placeablesList) {
+            if (button.TryGetComponent(out PlaceableButton unitButton)) {
                 unitButton.AddListener();
             } else {
                 Debug.LogWarning("UnitButton component missing on button: " + button.name);
             }
-        });
-
-        //FIX: Refactor for cleaner code
-        tabs.ForEach(tab => {
-
-            tab.onClick.AddListener(() => {
-                GameObject clickedObject = EventSystem.current.currentSelectedGameObject;
-                Button clickedButton = clickedObject.GetComponent<Button>();
-                RectTransform clickedRectTransform = clickedObject.GetComponent<RectTransform>();
-
-                tabIndex = tabs.FindIndex(button => button == clickedButton);
-
-                if (clickedRectTransform.sizeDelta.x == deSelectedWidth) {
-                    Deselect();
-                    DeactivateOtherContainers();
-                    ActivateLinkedContainer(tabIndex);
-
-                    Vector2 newSize = new Vector2(selectedWidth, clickedRectTransform.sizeDelta.y);
-                    clickedRectTransform.sizeDelta = newSize;
-                }
-            });
-
-        });
+        }
 
         placeablesContainers = GetValidContainers(placeablesContainer);
-
-        #region Activate Container One
-        DeactivateOtherContainers();
-        ActivateLinkedContainer(0);
-        #endregion
     }
 
-
-    private void Deselect()
+    private void OnTabSelected(Button clickedButton)
     {
-        tabs.ForEach((tab) => {
-            RectTransform allTabsRectTransform = tab.GetComponent<RectTransform>();
-
-            allTabsRectTransform.sizeDelta = new Vector2(deSelectedWidth, allTabsRectTransform.sizeDelta.y);
-        });
+        int tabIndex = tabs.IndexOf(clickedButton);
+        HandleTabSelection(tabIndex);
     }
 
-    private void DeactivateOtherContainers()
+    private void HandleTabSelection(int tabIndex)
     {
-        placeablesContainers.ForEach(container => {
+        if (tabIndex < 0 || tabIndex >= tabs.Count)
+            return;
 
-            container.gameObject.SetActive(false);
-        });
-    }
-    private void ActivateLinkedContainer(int i)
-    {
-        if (i >= 0 && i < placeablesContainers.Count) {
-            Transform selectedContainer = placeablesContainers[i];
+        RectTransform clickedRectTransform = tabs[tabIndex].GetComponent<RectTransform>();
 
-            selectedContainer.gameObject.SetActive(true);
+        if (clickedRectTransform.sizeDelta.x == DeSelectedWidth) {
+            DeselectAllTabs();
+            DeactivateAllContainers();
+            ActivateContainer(tabIndex);
+
+            clickedRectTransform.sizeDelta = new Vector2(SelectedWidth, clickedRectTransform.sizeDelta.y);
         }
     }
 
-    List<Transform> GetValidContainers(Transform parent)
+    private void DeselectAllTabs()
     {
-        List<Transform> containers = new List<Transform>();
+        foreach (var tab in tabs) {
+            RectTransform tabRectTransform = tab.GetComponent<RectTransform>();
+            tabRectTransform.sizeDelta = new Vector2(DeSelectedWidth, tabRectTransform.sizeDelta.y);
+        }
+    }
 
-        // Iterate through each child of the parent container
+    private void DeactivateAllContainers()
+    {
+        foreach (var container in placeablesContainers) {
+            container.gameObject.SetActive(false);
+        }
+    }
+
+    private void ActivateContainer(int index)
+    {
+        if (index >= 0 && index < placeablesContainers.Count) {
+            placeablesContainers[index].gameObject.SetActive(true);
+        }
+    }
+
+    private List<Transform> GetValidContainers(Transform parent)
+    {
+        var containers = new List<Transform>();
         foreach (Transform child in parent) {
-            // Check if the child has its own children
             if (child.childCount > 0) {
                 containers.Add(child);
             }
         }
-
         return containers;
     }
 }
